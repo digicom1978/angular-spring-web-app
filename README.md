e2962a07455458de017a11ce8054f5d9cb66b909
digicom1978 <digicom78@gmail.com>
2017-08-16 00:51:11 -0400

    End of Tutorial 10, simple session management and ui-routing

--- README.md
+++ README.md
@@ -27,3 +27,6 @@ Tutorial #8
 
 Tutorial #9
 - A tutorial covering ui-router, creation of a custom view and controller, and data-binding using $scope.
+
+Tutorial #10
+- covering simple session management using localStorage, creation of services using the factory method, and also some basic Angular and ui-router directives.
--- src/main/webapp/app/build/src/app/account/account.js
+++ src/main/webapp/app/build/src/app/account/account.js
@@ -18,16 +18,34 @@ angular.module('ngBoilerplate.account', ['ui.router'])
 				controller: 'RegisterCtrl'
 			}
 		},
-		data : {pageTitle : "Registeration 1"}
+		data : {pageTitle : "Registeration"}
 	});
 })
-.controller("LoginCtrl", function($scope) {
+.factory('sessionService', function() {
+	var session = {};
+	session.login = function(data) {
+		alert('User logged in with credentials ' + data.name + ' and  ' + data.password);
+		localStorage.setItem("session", data);
+	};
+	session.logout = function() {
+		localStorage.removeItem("session");
+	};
+	session.isLoggedIn = function() {
+		return localStorage.getItem("session") !== null;
+	};
+	return session;
+})
+.controller("LoginCtrl", function($scope, sessionService, $state) {
 	$scope.login = function() {
-		alert('user logged in with '+ $scope.account.name + ' and ' + $scope.account.password);
+//		alert('user logged in with '+ $scope.account.name + ' and ' + $scope.account.password);
+		sessionService.login($scope.account);
+		$state.go("home");
 	};
 })
-.controller("RegisterCtrl", function($scope) {
+.controller("RegisterCtrl", function($scope, sessionService, $state) {
 	$scope.register = function() {
-		alert('user registered with '+ $scope.account.name + ' and ' + $scope.account.password);
+//		alert('user registered with '+ $scope.account.name + ' and ' + $scope.account.password);
+		sessionService.login($scope.account);
+		$state.go("home");
 	};
 });
